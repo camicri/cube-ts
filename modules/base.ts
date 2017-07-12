b39549ba8bcda939d5319dbb0f5c39d3a9ad2aa6
Camicri <camicrisystems@gmail.com>
2017-07-12 22:48:02 +0800

    Removed unnecessary variables.

--- modules/base.ts
+++ modules/base.ts
@@ -2,6 +2,5 @@ import * as path from "path";
 
 export class BaseManager
 {
-    listDirectoryPath:string = path.join(__dirname,"lists");
-    sourcesDirectoryPath:string = path.join(__dirname,"sources"); 
+    
 }
