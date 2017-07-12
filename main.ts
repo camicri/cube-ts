7f32dac6625511ed353f78d77ab0fbaf2e4049ed
Camicri <camicrisystems@gmail.com>
2017-07-12 22:52:47 +0800

    Cleanup code. Added some basic Cube core usage

--- main.ts
+++ main.ts
@@ -1,41 +1,22 @@
-import {BaseManager} from "./modules/base";
-import {SourceManager} from "./modules/sources";
-import {Source} from "./modules/sources";
-import {Package} from "./modules/repositories";
-import {Project} from "./modules/project";
-import {RepositoryManager} from "./modules/repositories";
-import * as readLine from "linebyline";
-import * as fs from "fs";
 import * as path from "path";
+import {Cube} from "./modules/cube"
 
 export class Startup
 {
     public static main():number
     {
-        let project:Project = new Project("camicri", path.join(__dirname,"projects","camicri"));
-        let sourceMgr:SourceManager = new SourceManager(project);
-        let repositoryMgr:RepositoryManager = new RepositoryManager(project);
+        let cube:Cube = new Cube();
 
-        sourceMgr.scanRepositories();
-
-        let statusSource:Source = new Source("status");
-        statusSource.sourceFilePath = "installed";
-        statusSource.listFilePath = path.join(project.listsPath,"status");
-        statusSource.filename = "status";
-        sourceMgr.sources.push(statusSource);
-
-        repositoryMgr.scanRepositories(sourceMgr.sources).then(()=>{
-            console.log("Installed Packages: %d",Object.keys(repositoryMgr.installedPackages).length);
-            console.log("Available Packages: %d",Object.keys(repositoryMgr.availablePackages).length);
-            console.log("Provided Packages: %d",Object.keys(repositoryMgr.providedPackages).length);
-
-            console.log("Marking packages...");
-            repositoryMgr.markPackages();
-            console.log("Upgradable Packages: %d",repositoryMgr.upgradablePackages.length);            
+        cube.openProject(path.join(__dirname,"projects","camicri"))
+        .then(cube.scanRepositories.bind(cube))
+        .then(()=>{
+            cube.packageQuery.getSectionPackages("games").forEach((p)=>{
+                console.log(p.name)
+            });
         });
 
         return 0;
     }
 }
 
-Startup.main()
+Startup.main();
